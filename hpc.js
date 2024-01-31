f41a7f5cb935b53866d240fecbaed6816abac154
Pixel48 <piksel48@gmail.com>
2024-01-31 04:59:32 +0100

    HPC OK

--- hpc.js
+++ hpc.js
@@ -1,24 +1,22 @@
 export default class HPC {
   constructor(point, Nd, jacobian, cond) {
-    this.x = [];
-    this.y = [];
     this.result = [];
 
-    for (let _ = 0; _ < 4; _++) {
-      this.x.push([undefined, undefined, undefined, undefined]);
-      this.y.push([undefined, undefined, undefined, undefined]);
-      this.result.push([undefined, undefined, undefined, undefined]);
-    }
+    const { detJ } = jacobian;
+    // console.debug({ detJ });
+    // console.dir({ Nd }, { depth: null }); // OK
+
     for (let row = 0; row < 4; row++) {
       for (let col = 0; col < 4; col++) {
-        this.x[row][col] = Nd.dx[point][row] * Nd.dy[point][col];
-        this.y[row][col] = Nd.dy[point][row] * Nd.dx[point][col];
+        this.result[row] ??= [];
+
         this.result[row][col] =
+          cond *
           (Nd.dx[point][row] * Nd.dx[point][col] +
             Nd.dy[point][row] * Nd.dy[point][col]) *
-          jacobian.detJ *
-          cond;
+          detJ;
       }
     }
+    // console.dir(this.result, { depth: null });
   }
 }
