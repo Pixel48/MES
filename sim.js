42b77d5d8782a4b23393b9676b67a48f8543f070
Pixel48 <piksel48@gmail.com>
2024-01-31 17:42:40 +0100

    Sim deemed kinda-safe

--- sim.js
+++ sim.js
@@ -18,10 +18,12 @@ export default class Sim {
     // console.dir({ t0: this.t0 }, { depth: null });
   }
   step(output = false, border = false) {
+    /* #region logging */
     if (this._step == 0) {
       if (output) console.log(this.steps[this._step]);
       if (border) console.log(this.minMax(this.steps[this._step]));
     }
+    /* #endregion */
     this._step++;
 
     const HC = this.matrixSum(this.globalH.matrix, this.globalC.matrix);
@@ -30,23 +32,36 @@ export default class Sim {
       this.steps[this._step - 1]
     );
     const P = this.vectorSum(
-      this.globalH.P.map((value) => -value),
+      this.globalH.P, //.map((value) => -value),
       C
     );
+
+    // console.debug(`=== SIMULATION STEP (${this._step} / ${this.endStep}) ===`);
+    // console.dir({ /*HC,*/ P }, { depth: null });
 
     this.steps[this._step] = gaussianElimination(HC, P);
 
     /* #region logging */
     if (output) console.log(this.steps[this._step]);
-    if (border) console.log(this.minMax(this.steps[this._step]));
+    if (border)
+      console.log({
+        step: this._step,
+        minMax: this.minMax(this.steps[this._step]),
+      });
     /* #endregion */
   }
   run(output = false, border = false) {
+    if (output) console.debug('=== SIMULATION START ===');
     while (this._step < this.endStep) this.step(output, border);
   }
   matrixSum(matrix1, matrix2) {
-    if (matrix1.length != matrix2.length)
-      throw new Error('Matrix and vector must have the same length');
+    /* #region safeguard */
+    if (
+      matrix1.length != matrix2.length ||
+      matrix1[0].length != matrix2[0].length
+    )
+      throw new Error('Matrix and vector must have the same dimensions');
+    /* #endregion */
     const { length } = matrix1;
     const result = [];
     for (let row = 0; row < length; row++)
