2602b6b4eaf834277ca18ba8ee5266c5bee11528
Pixel48 <piksel48@gmail.com>
2024-01-31 17:10:00 +0100

    GlobalC deemed safe

--- globalc.js
+++ globalc.js
@@ -2,7 +2,7 @@ import { printMatrix } from './index.js';
 
 export default class GlobalC {
   constructor(C_, dataset) {
-    const { elements, nodes } = dataset;
+    const { elements, nodes, simStepTime } = dataset;
 
     this.matrix = [];
     for (let row = 0; row < nodes.length; row++) {
@@ -12,19 +12,20 @@ export default class GlobalC {
       }
     }
 
+    // console.debug({ simStepTime: dataset.simStepTime });
+
+    // elements.forEach((element, elementIndex) => {
     elements.forEach((element, elementIndex) => {
       const { nodes } = element;
-
-      elements.forEach((element, elementIndex) => {
-        for (let row = 0; row < 4; row++)
-          for (let col = 0; col < 4; col++) {
-            this.matrix[nodes[row].index][nodes[col].index] ??= 0;
-            this.matrix[nodes[row].index][nodes[col].index] +=
-              C_[elementIndex].matrix[row][col] / dataset.simStepTime;
-          }
-      });
+      for (let row = 0; row < 4; row++)
+        for (let col = 0; col < 4; col++) {
+          this.matrix[nodes[row].index][nodes[col].index] ??= 0;
+          this.matrix[nodes[row].index][nodes[col].index] +=
+            C_[elementIndex].matrix[row][col] / simStepTime;
+        }
     });
-    console.debug('\t\t### C GLOBAL MATRIX ###');
+    // });
+    console.debug('\t\t## C GLOBAL MATRIX ##');
     printMatrix(this.matrix);
   }
 }
